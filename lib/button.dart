e6641490696c6cd704a1d90fd483515410b91aaa
Koradia Yash <yashkoradia1302@gmail.com>
2024-03-03 17:50:16 +0530

    Initial commit

---
+++ lib/button.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String title;
+  final Color color;
+  final VoidCallback onPress;
+
+  const MyButton(
+      {super.key,
+      required this.title,
+      this.color = const Color(0xffa5a5a5),
+      required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Padding(
+        padding: const EdgeInsets.all(0.0),
+        child: Container(
+          height: 80,
+          width: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
